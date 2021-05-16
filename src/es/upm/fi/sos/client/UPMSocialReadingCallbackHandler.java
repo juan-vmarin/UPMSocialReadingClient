5417bf8a143153d46700c17134a11e322698599d
Juan <juan-vmarin@alumnos.upm.es>
2021-05-17 00:17:31 +0200

    Add interactive client

--- src/es/upm/fi/sos/client/UPMSocialReadingCallbackHandler.java
+++ src/es/upm/fi/sos/client/UPMSocialReadingCallbackHandler.java
@@ -1,4 +1,3 @@
-
 /**
  * UPMSocialReadingCallbackHandler.java
  *
@@ -6,207 +5,195 @@
  * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
  */
 
-    package es.upm.fi.sos.client;
+package es.upm.fi.sos.client;
 
-    /**
-     *  UPMSocialReadingCallbackHandler Callback class, Users can extend this class and implement
-     *  their own receiveResult and receiveError methods.
-     */
-    public abstract class UPMSocialReadingCallbackHandler{
+/**
+ * UPMSocialReadingCallbackHandler Callback class, Users can extend this class
+ * and implement their own receiveResult and receiveError methods.
+ */
+public abstract class UPMSocialReadingCallbackHandler {
 
+	protected Object clientData;
 
+	/**
+	 * User can pass in any object that needs to be accessed once the
+	 * NonBlocking Web service call is finished and appropriate method of this
+	 * CallBack is called.
+	 * 
+	 * @param clientData
+	 *            Object mechanism by which the user can pass in user data that
+	 *            will be avilable at the time this callback is called.
+	 */
+	public UPMSocialReadingCallbackHandler(Object clientData) {
+		this.clientData = clientData;
+	}
 
-    protected Object clientData;
+	/**
+	 * Please use this constructor if you don't want to set any clientData
+	 */
+	public UPMSocialReadingCallbackHandler() {
+		this.clientData = null;
+	}
 
-    /**
-    * User can pass in any object that needs to be accessed once the NonBlocking
-    * Web service call is finished and appropriate method of this CallBack is called.
-    * @param clientData Object mechanism by which the user can pass in user data
-    * that will be avilable at the time this callback is called.
-    */
-    public UPMSocialReadingCallbackHandler(Object clientData){
-        this.clientData = clientData;
-    }
+	/**
+	 * Get the client data
+	 */
 
-    /**
-    * Please use this constructor if you don't want to set any clientData
-    */
-    public UPMSocialReadingCallbackHandler(){
-        this.clientData = null;
-    }
+	public Object getClientData() {
+		return clientData;
+	}
 
-    /**
-     * Get the client data
-     */
+	// No methods generated for meps other than in-out
 
-     public Object getClientData() {
-        return clientData;
-     }
+	/**
+	 * auto generated Axis2 call back method for addFriend method override this
+	 * method for handling normal response from addFriend operation
+	 */
+	public void receiveResultaddFriend(
+			es.upm.fi.sos.client.UPMSocialReadingStub.AddFriendResponse result) {
+	}
 
-        
-               // No methods generated for meps other than in-out
-                
-           /**
-            * auto generated Axis2 call back method for addFriend method
-            * override this method for handling normal response from addFriend operation
-            */
-           public void receiveResultaddFriend(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.AddFriendResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from addFriend operation
+	 */
+	public void receiveErroraddFriend(java.lang.Exception e) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from addFriend operation
-           */
-            public void receiveErroraddFriend(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for removeFriend method
-            * override this method for handling normal response from removeFriend operation
-            */
-           public void receiveResultremoveFriend(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.RemoveFriendResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 call back method for removeFriend method override
+	 * this method for handling normal response from removeFriend operation
+	 */
+	public void receiveResultremoveFriend(
+			es.upm.fi.sos.client.UPMSocialReadingStub.RemoveFriendResponse result) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from removeFriend operation
-           */
-            public void receiveErrorremoveFriend(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for getMyFriendReadings method
-            * override this method for handling normal response from getMyFriendReadings operation
-            */
-           public void receiveResultgetMyFriendReadings(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.GetMyFriendReadingsResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from removeFriend operation
+	 */
+	public void receiveErrorremoveFriend(java.lang.Exception e) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from getMyFriendReadings operation
-           */
-            public void receiveErrorgetMyFriendReadings(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for getMyFriendList method
-            * override this method for handling normal response from getMyFriendList operation
-            */
-           public void receiveResultgetMyFriendList(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.GetMyFriendListResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 call back method for getMyFriendReadings method
+	 * override this method for handling normal response from
+	 * getMyFriendReadings operation
+	 */
+	public void receiveResultgetMyFriendReadings(
+			es.upm.fi.sos.client.UPMSocialReadingStub.GetMyFriendReadingsResponse result) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from getMyFriendList operation
-           */
-            public void receiveErrorgetMyFriendList(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for addUser method
-            * override this method for handling normal response from addUser operation
-            */
-           public void receiveResultaddUser(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.AddUserResponseE result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from getMyFriendReadings operation
+	 */
+	public void receiveErrorgetMyFriendReadings(java.lang.Exception e) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from addUser operation
-           */
-            public void receiveErroraddUser(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for removeUser method
-            * override this method for handling normal response from removeUser operation
-            */
-           public void receiveResultremoveUser(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.RemoveUserResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 call back method for getMyFriendList method override
+	 * this method for handling normal response from getMyFriendList operation
+	 */
+	public void receiveResultgetMyFriendList(
+			es.upm.fi.sos.client.UPMSocialReadingStub.GetMyFriendListResponse result) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from removeUser operation
-           */
-            public void receiveErrorremoveUser(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for getMyReadings method
-            * override this method for handling normal response from getMyReadings operation
-            */
-           public void receiveResultgetMyReadings(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.GetMyReadingsResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from getMyFriendList operation
+	 */
+	public void receiveErrorgetMyFriendList(java.lang.Exception e) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from getMyReadings operation
-           */
-            public void receiveErrorgetMyReadings(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for addReading method
-            * override this method for handling normal response from addReading operation
-            */
-           public void receiveResultaddReading(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.AddReadingResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 call back method for addUser method override this
+	 * method for handling normal response from addUser operation
+	 */
+	public void receiveResultaddUser(
+			es.upm.fi.sos.client.UPMSocialReadingStub.AddUserResponseE result) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from addReading operation
-           */
-            public void receiveErroraddReading(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for changePassword method
-            * override this method for handling normal response from changePassword operation
-            */
-           public void receiveResultchangePassword(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.ChangePasswordResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from addUser operation
+	 */
+	public void receiveErroraddUser(java.lang.Exception e) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from changePassword operation
-           */
-            public void receiveErrorchangePassword(java.lang.Exception e) {
-            }
-                
-           /**
-            * auto generated Axis2 call back method for login method
-            * override this method for handling normal response from login operation
-            */
-           public void receiveResultlogin(
-                    es.upm.fi.sos.client.UPMSocialReadingStub.LoginResponse result
-                        ) {
-           }
+	/**
+	 * auto generated Axis2 call back method for removeUser method override this
+	 * method for handling normal response from removeUser operation
+	 */
+	public void receiveResultremoveUser(
+			es.upm.fi.sos.client.UPMSocialReadingStub.RemoveUserResponse result) {
+	}
 
-          /**
-           * auto generated Axis2 Error handler
-           * override this method for handling error response from login operation
-           */
-            public void receiveErrorlogin(java.lang.Exception e) {
-            }
-                
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from removeUser operation
+	 */
+	public void receiveErrorremoveUser(java.lang.Exception e) {
+	}
 
+	/**
+	 * auto generated Axis2 call back method for getMyReadings method override
+	 * this method for handling normal response from getMyReadings operation
+	 */
+	public void receiveResultgetMyReadings(
+			es.upm.fi.sos.client.UPMSocialReadingStub.GetMyReadingsResponse result) {
+	}
 
-    }
-    
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from getMyReadings operation
+	 */
+	public void receiveErrorgetMyReadings(java.lang.Exception e) {
+	}
+
+	/**
+	 * auto generated Axis2 call back method for addReading method override this
+	 * method for handling normal response from addReading operation
+	 */
+	public void receiveResultaddReading(
+			es.upm.fi.sos.client.UPMSocialReadingStub.AddReadingResponse result) {
+	}
+
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from addReading operation
+	 */
+	public void receiveErroraddReading(java.lang.Exception e) {
+	}
+
+	/**
+	 * auto generated Axis2 call back method for changePassword method override
+	 * this method for handling normal response from changePassword operation
+	 */
+	public void receiveResultchangePassword(
+			es.upm.fi.sos.client.UPMSocialReadingStub.ChangePasswordResponse result) {
+	}
+
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from changePassword operation
+	 */
+	public void receiveErrorchangePassword(java.lang.Exception e) {
+	}
+
+	/**
+	 * auto generated Axis2 call back method for login method override this
+	 * method for handling normal response from login operation
+	 */
+	public void receiveResultlogin(
+			es.upm.fi.sos.client.UPMSocialReadingStub.LoginResponse result) {
+	}
+
+	/**
+	 * auto generated Axis2 Error handler override this method for handling
+	 * error response from login operation
+	 */
+	public void receiveErrorlogin(java.lang.Exception e) {
+	}
+
+}
--- src/es/upm/fi/sos/client/UPMSocialReadingClient.java
+++ src/es/upm/fi/sos/client/UPMSocialReadingClient.java
@@ -1,6 +1,9 @@
 package es.upm.fi.sos.client;
 
+import java.rmi.RemoteException;
 import java.util.Scanner;
+import es.upm.fi.sos.client.UPMSocialReadingStub.*;
+
 
 public class UPMSocialReadingClient {
 
@@ -14,14 +17,15 @@ public class UPMSocialReadingClient {
 		sc = new Scanner(System.in);
 
 		int option;
-		boolean exit = false;
-		while (!exit) {
-			System.out.println("1. Iniciar sesion\n" + "2. Añadir usuario\n"
-					+ "3. Eliminar usuario\n" + "4. Cambiar contraseña\n"
-					+ "5. Buscar usuario\n" + "6. Añadir amigo\n"
-					+ "7. Eliminar amigo\n" + "8. Lista de amigos\n"
-					+ "9. Añadir lectura\n" + "8. Lista de lecturas\n"
-					+ "9. Lecturas de amigo\n" + "10. Salir\n");
+		boolean finish = false;
+		while (!finish) {
+			System.out.println("1. Iniciar sesion\n" + "2. Cerrar sesion\n"
+					+ "3. Añadir usuario\n" + "4. Eliminar usuario\n"
+					+ "5. Cambiar contraseña\n" + "6. Buscar usuario\n"
+					+ "7. Añadir amigo\n" + "8. Eliminar amigo\n"
+					+ "9. Lista de amigos\n" + "10. Añadir lectura\n"
+					+ "11. Lista de lecturas\n" + "12. Lecturas de amigo\n"
+					+ "13. Salir\n");
 
 			option = sc.nextInt();
 			sc.nextLine();
@@ -31,42 +35,132 @@ public class UPMSocialReadingClient {
 				String user = sc.nextLine();
 				System.out.println("Introduzca su contrasena\n");
 				String pwd = sc.nextLine();
+				login(user, pwd, stub);
 				break;
 			case 2:
 				System.out.println("Introduzca el nombre de usuario\n");
-				System.out.println("Introduzca la contrasena\n");
-				pwd = sc.nextLine();
+				user = sc.nextLine();
+				logout(user, stub);
 				break;
 			case 3:
+				System.out.println("Introduzca el nombre del nuevo usuario\n");
+				String userNew = sc.nextLine();
+				addUser(userNew, stub);
 				break;
 			case 4:
-				System.out.println("Introduzca la contrasena actual\n");
-				String oldPwd = sc.nextLine();
-				System.out.println("Introduzca la nueva contrasena\n");
-				String newPwd = sc.nextLine();
+				removeUser(stub);
 				break;
 			case 5:
-
+				System.out.println("Introduzca la contrasena actual\n");
+				String actualPwd = sc.nextLine();
+				System.out.println("Introduzca la nueva contrasena\n");
+				String newPwd = sc.nextLine();
+				changePwd(actualPwd, newPwd, stub);
 				break;
 			case 6:
-
+				System.out.println("Introduzca el usuario que desea buscar\n");
+				String userSearch = sc.nextLine();
+				searchUser(userSearch, stub);
 				break;
 			case 7:
-
+				System.out.println("Introduzca el usuario que desea añadir\n");
+				String userAdd = sc.nextLine();
+				addFriend(userAdd, stub);
 				break;
 			case 8:
+				System.out
+						.println("Introduzca el usuario que desea eliminar\n");
+				String userDelete = sc.nextLine();
+				deleteFriend(userDelete, stub);
 				break;
 			case 9:
-
+				friendList(stub);
 				break;
 			case 10:
-
+				System.out.println("Introduzca la lectura que desea añadir\n");
+				String reading = sc.nextLine();
+				addReading(reading, stub);
+				finish = true;
+				break;
+			case 11:
+				readingList(stub);
+				break;
+			case 12:
+				System.out
+						.println("Introduzca el usuario del cual desea ver las lecturas\n");
+				String userReadings = sc.nextLine();
+				friendReadings(userReadings, stub);
+				break;
+			case 13:
+				finish = true;
 				break;
 			default:
 				break;
-			}
+			} // end switch
 
-		}
+		} // end while
+		sc.close();
+	}
+
+	private static void login(String user, String pwd, UPMSocialReadingStub stub)
+			throws RemoteException { // 1
+
+	}
+
+	private static void logout(String user, UPMSocialReadingStub stub)
+			throws RemoteException { // 2
+		Logout logout = new Logout();
+		stub.logout(logout);
+		return;
+	}
+
+	private static void addUser(String user, UPMSocialReadingStub stub)
+			throws RemoteException { // 3
+
+	}
+
+	private static void removeUser(UPMSocialReadingStub stub)
+			throws RemoteException { // 4
+
+	}
+
+	private static void changePwd(String actualPwd, String newPwd,
+			UPMSocialReadingStub stub) throws RemoteException { // 5
+
+	}
+
+	private static void searchUser(String user, UPMSocialReadingStub stub)
+			throws RemoteException { // 6
+
+	}
+
+	private static void addFriend(String user, UPMSocialReadingStub stub)
+			throws RemoteException { // 7
+
+	}
+
+	private static void deleteFriend(String user, UPMSocialReadingStub stub)
+			throws RemoteException { // 8
+
+	}
+
+	private static void friendList(UPMSocialReadingStub stub)
+			throws RemoteException { // 9
+
+	}
+
+	private static void addReading(String reading, UPMSocialReadingStub stub)
+			throws RemoteException { // 10
+
+	}
+
+	private static void readingList(UPMSocialReadingStub stub)
+			throws RemoteException { // 11
+
+	}
+
+	private static void friendReadings(String user, UPMSocialReadingStub stub)
+			throws RemoteException { // 12
 
 	}
 
